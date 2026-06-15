630fc40ffe5f9008dec186bd02b9b54871b62caf
Felipe Braga <febragasilva@outlook.com>
2026-06-15 11:54:34 -0300

    novos passos...

---
+++ teste/Snake-Etapa2.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <SDL3/SDL.h>
+
+int main(int argc, char* argv[])
+{
+    //1. Criação da janela
+    if(!SDL_Init(SDL_INIT_VIDEO))
+    {
+        printf("Erro: %s \n", SDL_GetError());
+        return 1;
+    }
+
+    SDL_Window* window = SDL_CreateWindow(
+                                          "Snake SDL3 - Etapa 2", //1. Título
+                                          640, 480,                          //2. Largura x altura em pixels
+                                          0                                         //3.Flag (0 = normal);
+                                          );
+    if(!window)
+    {
+        printf("Erro: %s \n", SDL_GetError());
+        SDL_Quit();
+        return 1;
+    }
+
+    //2.Criação do renderer
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, NULL);
+
+    if(!renderer)
+    {
+        printf("Erro: %s \n", SDL_GetError());
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+
+    bool running = true;
+    SDL_Event event;
+
+    while(running)
+    {
+        //--- Eventos ---
+        while(SDL_PollEvent(&event))
+        {
+            if(event.type == SDL_EVENT_QUIT)
+                running = false;
+        }
+    //--- Fundo preto ---
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    SDL_RenderClear(renderer);
+
+    // --- Desenhando retângulos ---
+
+    //SDL_Rect: retângulo com floats {x, y, largura, altura}
+
+    //Bloco verde (simulando um pedaço da cobra)
+    SDL_FRect bloco_cobra = {100.0f, 100.0f, 40.0f, 40.0f};
+    SDL_SetRenderDrawColor(renderer, 0, 200, 0, 255); //Verde
+    SDL_RenderFillRect(renderer, &bloco_cobra);
+
+    //Bloco vermelho
+    SDL_FRect bloco_comida = {300.0f, 200.0f, 40.0f, 40.0f};
+    SDL_SetRenderDrawColor(renderer, 200, 0, 0, 255); //vermelho
+    SDL_RenderFillRect(renderer, &bloco_comida);
+
+    //--- Apresenta o frame ---
+    SDL_RenderPresent(renderer);
+
+    }
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    return 0;
+}
